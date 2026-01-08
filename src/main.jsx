acb19d704e447ef1ad3f3ea3d7e89fe72f0ce7a9
Noah120833 <jonnykaeptain2@gmail.com>
2026-01-08 22:25:30 +0100

    Remove unused React import from main.jsx
    Removed unused import of React in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import React from 'react';
 // 🗑️ Delete the Sidebar import line that was right here
 import App from './App.jsx'
 
