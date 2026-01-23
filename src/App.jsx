abf8fd0c85817525e1d3336da2bd5a8dffaeae8a
Noah120833 <jonnykaeptain2@gmail.com>
2026-01-23 20:41:35 +0100

    Fix import statement formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect} from 'react' 
+import React, { useState, useRef, useEffect } from 'react'
 import Markdown from 'react-markdown'// Necessary to use states
 import './App.css'          
 
